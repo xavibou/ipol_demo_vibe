b75c7cc89bcc27f07f8dc68709244048e8662435
Xavi Bou <xavibou@Xavis-MacBook-Pro.local>
2022-09-12 16:56:51 +0200

    switch to video input - 1

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 import os
 import iio
 import numpy as np
+import cv2
 
 def main(input, output, sample_num, radius, min_matches, update_factor):
-    u = iio.read(input)
-    print("hello world", u.shape)
+    #u = iio.read(input)
+    v = cv2.VideoCapture(input)
+
+    #print("hello world", u.shape)
     print("sample_num: {}".format(sample_num))
     print("radius: {}".format(radius))
     print("min_matches: {}".format(min_matches))
@@ -15,9 +18,15 @@ def main(input, output, sample_num, radius, min_matches, update_factor):
     #iio.write(output, v)
 
 if __name__ == "__main__":
-    input = "input_0.png"
+
+    # Read input and parameters
+    input = "input_0.mp4"
     sample_num = int(os.environ['sample_num'])
     radius = int(os.environ['radius'])
     min_matches = int(os.environ['min_matches'])
     update_factor = int(os.environ['update_factor'])
+
+    # Check parameter errors
+
+    # Run demo
     main("input_0.png", "output.png", sample_num, radius, min_matches, update_factor)
