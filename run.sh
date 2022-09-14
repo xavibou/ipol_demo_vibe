52482f0cdb5e5526eec0fc15db32f2c5e672c81f
Xavi Bou <xavibou@w-154-53.wfer.ens-paris-saclay.fr>
2022-09-14 13:11:00 +0200

    change to mp4 format videos

--- run.sh
+++ run.sh
@@ -19,8 +19,8 @@ ffmpeg -i $1 imdir/out%08d.png
 # Move output files on a new directory and generate the two sequences
 mkdir masks
 mv imdir/*mask.png masks
-ffmpeg -framerate 1 -pattern_type glob -i "imdir/*.png" -pix_fmt yuv420p input.mov
-ffmpeg -framerate 1 -pattern_type glob -i "masks/*.png" -pix_fmt yuv420p -vf scale="iw*4:ih*4" output.mov
+ffmpeg -framerate 1 -pattern_type glob -i "imdir/*.png" -pix_fmt yuv420p input.mp4
+ffmpeg -framerate 1 -pattern_type glob -i "masks/*.png" -pix_fmt yuv420p -vf scale="iw*4:ih*4" output.mp4
 
 # Zip output dir
 zip -r masks.zip masks
