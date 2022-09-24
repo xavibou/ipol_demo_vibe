2d4ad1dc77e3972822aabb772191c03d2bbb72ee
Xavi Bou <xavibou@Xavis-MacBook-Pro.local>
2022-09-24 12:38:43 +0200

    code commented

--- main.c
+++ main.c
@@ -1,6 +1,6 @@
 /*----------------------------------------------------------------------------
 
-  Copyright (c) 2018-2022 rafael grompone von gioi <grompone@gmail.com>
+  Copyright (c) 2018-2022 Xavier Bou Hernandez <xavibouhae@gmail.com>
 
   This program is free software: you can redistribute it and/or modify
   it under the terms of the GNU Affero General Public License as
